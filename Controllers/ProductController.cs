f4a48f317075a19de1f65d508dc0c6e64e18c1dc
gustavo acuña <gustavo.acuna@innod-tech.com>
2023-12-25 21:21:15 -0300

    added entity framework dependencies

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web_store_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public ProductController(ISender sender)
+        {
+            _sender = sender;
+        }
+    }
+}
