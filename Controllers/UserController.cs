599902fd35f9ab4c8bb82cdc80b7db688da5b65e
Gustavo Acuna <gustavo.acu.rivas@gmail.com>
2024-05-26 22:27:44 -0400

    added remove user feature at controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
 using web_store_server.Domain.Communication;
 using web_store_server.Domain.Dtos.Users;
 using web_store_server.Features.Users.Commands;
@@ -10,7 +11,7 @@ namespace web_store_server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    //[Authorize]
+    [Authorize]
     public class UserController : ControllerBase
     {
         private readonly ISender _sender;
@@ -22,6 +23,11 @@ namespace web_store_server.Controllers
             _APIResultHandler = APIResultHandler;
         }
 
+        /// <summary>
+        /// Permite listar los usuarios
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
         [HttpGet]
         public async Task<ActionResult<DefaultAPIResponse<UserDto>>> GetUsersList(
             CancellationToken token)
@@ -38,6 +44,12 @@ namespace web_store_server.Controllers
                 });
         }
 
+        /// <summary>
+        /// Permite crear un usuario
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
         [HttpPost]
         public async Task<ActionResult<DefaultAPIResponse<Guid>>> CreateUsers(
             [FromBody] CreateUpdateUserDto user,
@@ -64,6 +76,13 @@ namespace web_store_server.Controllers
                 });
         }
 
+        /// <summary>
+        /// Permite actualizar un usuario
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="user"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
         [HttpPut("{userId:Guid}")]
         public async Task<ActionResult<DefaultAPIResponse<CreateUpdateUserDto?>>> UpdateUsers(
             [FromRoute] Guid userId,
@@ -88,6 +107,38 @@ namespace web_store_server.Controllers
                     Message = result.Message,
                     IsSuccess = false,
                     Data = result.Data
+                });
+        }
+
+        /// <summary>
+        /// Permite eliminar un usuario
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        [HttpDelete("{userId:Guid}")]
+        public async Task<ActionResult<DefaultAPIResponse<AnyType>>> RemoveUser(
+            [FromRoute] Guid userId,
+            CancellationToken token)
+        {
+            var result = await _sender.Send(new DeleteUserCommand(userId), token);
+
+            return result.IsSuccess ?
+                _APIResultHandler.HandleDefaultResponse(
+                StatusCodes.Status200OK,
+                new DefaultAPIResponse<CreateUpdateUserDto?>()
+                {
+                    Message = "Usuario eliminado exitosamente.",
+                    IsSuccess = true,
+                    Data = null
+                }) :
+                _APIResultHandler.HandleDefaultResponse(
+                StatusCodes.Status400BadRequest,
+                new DefaultAPIResponse<CreateUpdateUserDto?>()
+                {
+                    Message = result.Message,
+                    IsSuccess = false,
+                    Data = null
                 });
         }
     }
